584bbe73101674b72d72a4c663712ff711cb41fd
TerribleCodes <madawadesilva9998@gmail.com>
2023-05-29 06:37:06 +0530

    Initial Commit

--- server/src/models/Users.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const userModel = mongoose.model(
-  "users",
-  new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  })
-);
